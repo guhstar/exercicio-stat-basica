8d5c36f1196ec92c98c38665468981c7727cd13c
Gustavo <guhdutra21@usp.br>
2026-03-22 22:54:23 -0300

    vossoro

--- analise.R
+++ analise.R
@@ -1,12 +1,6 @@
 # Análise simples
 x <- 1:10
 mean
-git config --global user.name "Seu Nome"
-git config --global user.email "seu.email@exemplo.com"
-install.packages("usethis")
 library(usethis)
 usethis::create_github_token()
-install.packages("gitcreds")
 gitcreds::gitcreds_set()
-git config --global user.name "Gustavo"
-git config --global user.email "guhdutra21@usp.br"
